e22c6603d78de9aafb34dceef02325c0a165cc69
Florian Felten <>
2021-08-30 14:12:52 +0200

    Fix test HV

--- PQL/test/test_hv_indicator.py
+++ PQL/test/test_hv_indicator.py
@@ -1,14 +1,15 @@
 import numpy as np
 import pytest
+from PQL.utils.QSet import QSet
 
 from PQL.utils.hv_indicator import MaxHVHeuristic
 
 test_data = [
-    (MaxHVHeuristic(ref_point=np.array([10.0, 0.0])), np.array([[[1, -0.3]], [[2, -0.2]], [[0.2, -0.5]]]), 2),
-    (MaxHVHeuristic(ref_point=np.array([10.0, 10.0])), np.array([[[1, 2]], [[2, 1], [1, 1]], [[0, 2]]]), 2),
-    (MaxHVHeuristic(ref_point=np.array([10.0, 10.0, 10.0])), np.array([[[1, 2, 2]], [[2, 1, 2]], [[0, 2, 2]]]), 2)
+    (MaxHVHeuristic(ref_point=np.array([10.0, 10.0])), np.array([QSet([[1, -0.3]]), QSet([[2, -0.2]]), QSet([[0.2, -0.5]])]), 2),
+    (MaxHVHeuristic(ref_point=np.array([10.0, 10.0])), np.array([QSet([[1, 2]]), QSet([[2, 1], [1, 2]]), QSet([[0, 2]])]), 1),
+    (MaxHVHeuristic(ref_point=np.array([10.0, 10.0, 10.0])), np.array([QSet([[1, 2, 2]]), QSet([[2, 1, 2]]), QSet([[0, 2, 2]])]), 2)
 ]
 
 @pytest.mark.parametrize("hv, qsets, expected_action", test_data)
 def test_hv(hv, qsets, expected_action):
-    assert hv.compute(qsets) == expected_action
+    assert hv.compute(qsets).argmax() == expected_action
